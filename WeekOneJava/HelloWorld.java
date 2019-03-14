4aee6573024fb47578eb7d6c4ea7ddac2e4df5c5
Blake Thomas <blakethomas0101@gmail.com>
2019-03-14 12:36:05 -0400

    Add files via upload

---
+++ WeekOneJava/HelloWorld.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+
+/**
+ * @author ThomasBA21
+ *
+ */
+public class HelloWorld {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// Prints "Hello World!" to the screen
+				System.out.println("Hello World!");
+	}
+
+}
