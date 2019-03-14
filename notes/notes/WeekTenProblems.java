6bac6d86acd17671eb89bdcd4dcdc70e1dd64024
Blake Thomas <blakethomas0101@gmail.com>
2019-03-14 16:26:41 -0400

    notes edit into working java

---
+++ notes/notes/WeekTenProblems.java
@@ -0,0 +1,16 @@
+package notes;
+
+public class WeekTenProblems {
+	
+	public static void main(String[] args) {
+		System.out.print("Here we go!");
+		System.out.print("12345");
+		System.out.print("Test this if you aren't sure.");
+		System.out.print("Another");
+		System.out.print("");
+		System.out.print("All done. \n");
+		System.out.print("50 plus 25 is " + (25+50));
+
+	}
+
+}
