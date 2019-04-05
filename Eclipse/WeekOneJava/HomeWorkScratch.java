f0982a743dd4736e7f2aa94df7f7e3cca98f953a
Thomas <ThomasBA21@ccescavs.org>
2019-04-05 09:42:43 -0400

    sync

--- Eclipse/WeekOneJava/HomeWorkScratch.java
+++ Eclipse/WeekOneJava/HomeWorkScratch.java
@@ -1,9 +1,18 @@
-
+import java.util.Scanner;
 public class HomeWorkScratch {
 
+	private static Scanner scan;
+
 	public static void main(String[] args) {
-		System.out.println("\"He thrusts his fists \n\t against the post \n\t\t and still insists \n\t\t\t he sees the 'ghost'\"");
-		
+		scan = new Scanner (System.in);
+		System.out.println("Enter an integer:");
+		int max = scan.nextInt ();
+		for (int i=1; i <=9; i++){
+			System.out.println("Enter an integer:");
+			int number = scan.nextInt ();
+			if  (number > max) max = number;
+		}
+		System.out.println("The largest integer entered was:" + max);
 	}
 
 }
